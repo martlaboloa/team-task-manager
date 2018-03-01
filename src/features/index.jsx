d4179e51d534e7d5272c08c8e7a51382600850c9
Shalia <sharoshidze@loankeeper.ge>
2018-03-01 19:41:36 +0400

    added material in deps

---
+++ src/features/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class TeamTaskManager extends React.PureComponent {
+    render() {
+        return (
+            <div>team task manager</div>
+        )
+    }
+}
+
+export default TeamTaskManager
