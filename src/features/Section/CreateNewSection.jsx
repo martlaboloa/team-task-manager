ee98fdee9ddd4aef8e48b59d749cf75a408b9f8d
Shalia <sharoshidze@loankeeper.ge>
2018-03-04 15:41:56 +0400

    creating section properly from input

--- src/features/Section/CreateNewSection.jsx
+++ src/features/Section/CreateNewSection.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import FlatButton from 'material-ui/FlatButton'
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import TextField from 'material-ui/TextField'
+import IconButton from 'material-ui/IconButton'
+import ImageLens from 'material-ui/svg-icons/image/lens'
 import actions from '../store/actions'
 
 const btnStyles = {
@@ -18,25 +21,65 @@ const childrenContStyles = {
 }
 
 class CreateNewSection extends Component {
-    onClick = () => {
-        const { addSection } = this.props
+    state = { creatingSection: false }
 
-        addSection('Section Name')
+    onClickCreateBtn = () => {
+        this.setState({ creatingSection: true })
+    }
+
+    onKeyDownCreatingField = (e) => {
+        const { keyCode, target: { value } } = e
+
+        if (keyCode === 13 && value !== null && value !== undefined && value !== '') {
+            const { addSection } = this.props
+
+            addSection(value)
+
+            this.setState({ creatingSection: false })
+        }
+    }
+
+    onBlurCreatingField = (e) => {
+        const { target: { value } } = e
+
+        if (value !== null && value !== undefined && value !== '') {
+            const { addSection } = this.props
+
+            addSection(value)
+
+        }
+
+        this.setState({ creatingSection: false })
     }
 
     render() {
-        return (
-            <div className="create-new-section-btn" >
+        return [
+            this.state.creatingSection ?
+                <div key="creatingSection" className="section-container">
+                    <IconButton >
+                        <ImageLens color={'#ff0000'} viewBox="0 0 50 20" />
+                    </IconButton>
+
+                    <TextField
+                        hintText="Section name"
+                        autoFocus
+                        onKeyDown={this.onKeyDownCreatingField}
+                        onBlur={this.onBlurCreatingField}
+                    />
+                </div>
+                : null,
+            <div key="createSecBtn" className="create-new-section-btn" >
                 <FlatButton
                     style={btnStyles}
-                    onClick={this.onClick}
+                    onClick={this.onClickCreateBtn}
                 >
                     <div style={childrenContStyles}>
                         <ContentAdd color="#747474" style={{ margin: '5px' }} />New Section
                     </div>
                 </FlatButton>
-            </div>
-        )
+            </div>,
+        ]
+
     }
 }
 
