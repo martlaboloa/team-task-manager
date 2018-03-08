e49b366af52e504b133653381a6112eba9ebc788
Shalia <sharoshidze@loankeeper.ge>
2018-03-08 04:16:04 +0400

    favicon changed tabs added

--- src/features/index.jsx
+++ src/features/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { DragDropContext } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend';
-import Paper from 'material-ui/Paper'
+import HTML5Backend from 'react-dnd-html5-backend'
 import Drawer from 'material-ui/Drawer'
+import AppBar from 'material-ui/AppBar'
+import {Tabs, Tab} from 'material-ui/Tabs';
 import Board from './Board'
 import './styles.css'
 
@@ -10,16 +11,32 @@ class TeamTaskManager extends React.PureComponent {
     render() {
         return (
             <div className="ttm-container">
-                <div className="header" />
+                <AppBar
+                    title="Title"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                />
+
+                <Tabs>
+                    <Tab label="Item One" >
+                        aaa
+                    </Tab>
+                    <Tab label="Item Two" >
+                        bbb
+                    </Tab>
+                    <Tab label="onActive" >
+                        ccc
+                    </Tab>
+                </Tabs>
+
+                <Drawer open={true} containerClassName="drawer-container" className="drawer" >
+                    aaaa
+                </Drawer>
 
                 <div className="content-container">
-                    <Drawer open={true} containerClassName="drawer-container" className="drawer" >
-                        aaaa
-                    </Drawer>
 
-                    <Paper className="board-wrapper" zDepth={2}>
-                        <Board />
-                    </Paper>
+                    <div className="board-wrapper" zDepth={2}>
+
+                    </div>
                 </div>
             </div>
         )
